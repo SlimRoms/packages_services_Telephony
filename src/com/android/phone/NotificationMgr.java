dc9a703886d5deada3e6c4ba12d8bcb5afa75c39
Danny Baumann <dannybaumann@web.de>
2013-11-07 16:51:36 +0100

    Add back phone notification LED settings backend (2/2)
    Change-Id: I1f6834872ac8320e43a0c5d77b1dc8d3654fa99b

--- src/com/android/phone/NotificationMgr.java
+++ src/com/android/phone/NotificationMgr.java
@@ -21,6 +21,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.StatusBarManager;
 import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -62,6 +63,8 @@ import java.util.Set;
 
 import org.codeaurora.internal.IExtTelephony;
 
+import slim.provider.SlimSettings;
+
 /**
  * NotificationManager-related utility code for the Phone app.
  *
@@ -90,6 +93,10 @@ public class NotificationMgr {
     static final int SELECTED_OPERATOR_FAIL_NOTIFICATION = 6;
 
     static final int NOTIFICATION_ID_OFFSET = 50;
+
+    // notification light default constants
+    public static final int DEFAULT_COLOR = 0xFFFFFF; //White
+    public static final int DEFAULT_TIME = 1000; // 1 second
 
     /** The singleton NotificationMgr instance. */
     private static NotificationMgr sInstance;
@@ -179,6 +186,37 @@ public class NotificationMgr {
             return sInstance;
         }
     }
+
+    /**
+     * Configures a Notification to emit the blinky Voice mail
+     * signal.
+     */
+    private static void configureLedNotification(Context context, Notification notification) {
+        ContentResolver resolver = context.getContentResolver();
+
+        boolean lightEnabled = SlimSettings.System.getInt(resolver,
+                SlimSettings.System.NOTIFICATION_LIGHT_PULSE, 0) == 1;
+        if (!lightEnabled) {
+            return;
+        }
+
+        notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+
+        // Get Voice mail values if they are to be used
+        boolean customEnabled = SlimSettings.System.getInt(resolver,
+                SlimSettings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_ENABLE, 0) == 1;
+        if (!customEnabled) {
+            notification.defaults |= Notification.DEFAULT_LIGHTS;
+            return;
+        }
+
+        notification.ledARGB = SlimSettings.System.getInt(resolver,
+            SlimSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_COLOR, DEFAULT_COLOR);
+        notification.ledOnMS = SlimSettings.System.getInt(resolver,
+            SlimSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_LED_ON, DEFAULT_TIME);
+        notification.ledOffMS = SlimSettings.System.getInt(resolver,
+            SlimSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_LED_OFF, DEFAULT_TIME);
+     }
 
     /** The projection to use when querying the phones table */
     static final String[] PHONES_PROJECTION = new String[] {
@@ -405,6 +443,7 @@ public class NotificationMgr {
                         && !user.isManagedProfile()) {
                     if (!sendNotificationCustomComponent(vmCount, vmNumber, pendingIntent,
                             isSettingsIntent)) {
+                        configureLedNotification(mContext, notification);
                         mNotificationManager.notifyAsUser(
                                 Integer.toString(subId) /* tag */,
                                 notificationId,
