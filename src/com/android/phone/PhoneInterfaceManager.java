e0cb46571775fad0bb43730e3cd81ba4220e0a6c
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-04-28 11:42:05 +0100

    NetworkSettings: Don't error out with TD-SCDMA values
    This adds persistence and the ability to read TD-SCDMA values if
    they are set. Both Network configuration and the QS tile toggle
    now cycle these modes correctly
    Devices supporting TD-SCDMA need to overlay the config_enabled_tdscdma
    boolean configuration value to "true" for the proper network modes
    to be presented to the user.
    
    The ro.telephony.default_network should also be changed to a TD-SCDMA
    value (20 or 18, depending on whether LTE is also supported)
    
    Change-Id: I3a88a01aa9c5c111101c53388d5ba76a99cd0df6

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -370,6 +370,13 @@ public class PhoneInterfaceManager extends ITelephony.Stub implements CallModele
         case Phone.NT_MODE_LTE_CDMA_AND_EVDO:
             network = Phone.NT_MODE_CDMA;
             break;
+        // TD-SCDMA Devices
+        case Phone.NT_MODE_TD_SCDMA_GSM_WCDMA:
+            network = Phone.NT_MODE_TD_SCDMA_GSM_WCDMA_LTE;
+            break;
+        case Phone.NT_MODE_TD_SCDMA_GSM_WCDMA_LTE:
+            network = Phone.NT_MODE_TD_SCDMA_GSM_WCDMA;
+            break;
         }
 
         mPhone.setPreferredNetworkType(network,
