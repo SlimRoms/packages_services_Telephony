fdda2a19e6decde31d316dfdde62f3c41621c22c
ajrty33 <ajrty33@gmail.com>
2014-11-29 19:25:14 -0600

    Telephony: Make voicemail notifications dismissable
    Change-Id: I022caa76907a001d70826de4bd22e7dc9bb18405

--- src/com/android/phone/NotificationMgr.java
+++ src/com/android/phone/NotificationMgr.java
@@ -533,7 +533,7 @@ public class NotificationMgr {
                     .setContentIntent(pendingIntent)
                     .setSound(ringtoneUri)
                     .setColor(mContext.getResources().getColor(R.color.dialer_theme_color))
-                    .setOngoing(true);
+                    .setOngoing(false);
 
             CallFeaturesSetting.migrateVoicemailVibrationSettingsIfNeeded(prefs, phoneId);
             final boolean vibrate = prefs.getBoolean(
