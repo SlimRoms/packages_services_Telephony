7015bf7f57cfab264b38ef270570df9bdfe52cd1
Marcos Marado <mmarado@cyngn.com>
2015-01-21 20:17:54 +0000

    MSIM Network Settings update
    * Material theme
    * CheckBox -> Switch
    * Use iterator instead of name for tabs (name is not unique)
    
    Change-Id: I64a452ac52d85401329ee00fda427d8661b2df9c

--- src/com/android/phone/msim/SelectSubscription.java
+++ src/com/android/phone/msim/SelectSubscription.java
@@ -47,7 +47,7 @@ import com.android.phone.R;
 
 import java.util.List;
 
-public class SelectSubscription extends  TabActivity {
+public class SelectSubscription extends TabActivity {
 
     private static final String LOG_TAG = "SelectSubscription";
     private static final boolean DBG = (PhoneGlobals.DBG_LEVEL >= 2);
@@ -93,9 +93,7 @@ public class SelectSubscription extends  TabActivity {
 
             log("Creating SelectSub activity = " + i + " displayName = " + displayName);
 
-            // Add phone Id to the displayName to differentiate
-            // tag names of tabs.
-            subscriptionPref = tabHost.newTabSpec(displayName + i);
+            subscriptionPref = tabHost.newTabSpec(Integer.toString(i));
             subscriptionPref.setIndicator(displayName);
             intent = new Intent().setClassName(pkg, targetClass)
                     .setAction(intent.getAction());
