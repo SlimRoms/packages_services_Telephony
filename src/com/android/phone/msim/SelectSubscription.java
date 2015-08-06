CyanogenMod/android_packages_services_Telephony

MSIM: Replace the "SUB" string
Change-Id: Iefc6876b1aed383a56de1e5f44d7c384c90a6885

src/com/android/phone/msim/SelectSubscription.java
@@ -52,7 +52,7 @@ public class SelectSubscription extends  TabActivity {
     public static final String PACKAGE = "PACKAGE";
     public static final String TARGET_CLASS = "TARGET_CLASS";
 
-    private String[] tabLabel = {"SUB 1", "SUB 2", "SUB 3"};
+    private String[] tabLabel = {"SIM 1", "SIM 2", "SIN 3"};
 
     private TabSpec subscriptionPref;
 

SlimRoms/packages_services_Telephony

Change-Id: Ia00ea805775038bd541183dc04ababc2d8201cc4

src/com/android/phone/msim/SelectSubscription.java
@@ -53,7 +53,7 @@ public class SelectSubscription extends  TabActivity {
     public static final String PACKAGE = "PACKAGE";
     public static final String TARGET_CLASS = "TARGET_CLASS";
 
-    private String[] tabLabel = {"SUB 1", "SUB 2", "SUB 3"};
+    private String[] tabLabel = {"SIM 1", "SIM 2", "SIM 3"};
 
     private TabSpec subscriptionPref;
 