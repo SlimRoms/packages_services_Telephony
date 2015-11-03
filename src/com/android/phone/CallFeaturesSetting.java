569f29a9d02c4b5598932812cbeccf9abbe0c372
dhacker29 <dhackerdvm@gmail.com>
2015-11-02 23:50:36 -0500

    CallFeatureSettings: Only show video quality if QTI package installed
    Shamu uses org.codeaurora.ims package and different constants for video
    quality settings, so this crashes on open.
    
    Change-Id: I701635d475a85c16556e716606ec59aad24e8948

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -63,6 +63,8 @@ import java.lang.String;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slim.utils.PackageManagerUtils;
+
 /**
  * Top level "Call settings" UI; see res/xml/call_feature_setting.xml
  *
@@ -351,6 +353,10 @@ public class CallFeaturesSetting extends PreferenceActivity
                         CarrierConfigManager.KEY_CARRIER_VOLTE_TTY_SUPPORTED_BOOL)) {
             TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
             /* tm.listen(mPhoneStateListener, PhoneStateListener.LISTEN_CALL_STATE); */
+        }
+
+        if (!PackageManagerUtils.isAppInstalled(this, "com.qualcomm.qti.ims")) {
+            prefSet.removePreference(findPreference("ims_settings_key"));
         }
 
         Preference wifiCallingSettings = findPreference(
