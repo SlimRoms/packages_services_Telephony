69a763a006267aa1b5b1f2fd64fddd9bf4102c67
Bryce Lee <brycelee@google.com>
2015-12-04 15:44:16 -0800

    Remove BluetoothManager.
    Bug: 25851538
    Change-Id: I40cbd1fc5c26fb61bdf815a5ecc044197d887a0e

--- src/com/android/phone/CallNotifier.java
+++ src/com/android/phone/CallNotifier.java
@@ -123,7 +123,6 @@ public class CallNotifier extends Handler {
 
     // Cached AudioManager
     private AudioManager mAudioManager;
-    private final BluetoothManager mBluetoothManager;
     private SubscriptionManager mSubscriptionManager;
     private TelephonyManager mTelephonyManager;
 
@@ -156,11 +155,10 @@ public class CallNotifier extends Handler {
     /* package */ static CallNotifier init(
             PhoneGlobals app,
             CallLogger callLogger,
-            CallStateMonitor callStateMonitor,
-            BluetoothManager bluetoothManager) {
+            CallStateMonitor callStateMonitor) {
         synchronized (CallNotifier.class) {
             if (sInstance == null) {
-                sInstance = new CallNotifier(app, callLogger, callStateMonitor, bluetoothManager);
+                sInstance = new CallNotifier(app, callLogger, callStateMonitor);
             } else {
                 Log.wtf(LOG_TAG, "init() called multiple times!  sInstance = " + sInstance);
             }
@@ -172,12 +170,10 @@ public class CallNotifier extends Handler {
     private CallNotifier(
             PhoneGlobals app,
             CallLogger callLogger,
-            CallStateMonitor callStateMonitor,
-            BluetoothManager bluetoothManager) {
+            CallStateMonitor callStateMonitor) {
         mApplication = app;
         mCM = app.mCM;
         mCallLogger = callLogger;
-        mBluetoothManager = bluetoothManager;
 
         mAudioManager = (AudioManager) mApplication.getSystemService(Context.AUDIO_SERVICE);
         mTelephonyManager =
@@ -490,13 +486,6 @@ public class CallNotifier extends Handler {
             }
             mPreviousCdmaCallState = fgPhone.getForegroundCall().getState();
         }
-
-        // Have the PhoneApp recompute its mShowBluetoothIndication
-        // flag based on the (new) telephony state.
-        // There's no need to force a UI update since we update the
-        // in-call notification ourselves (below), and the InCallScreen
-        // listens for phone state changes itself.
-        mBluetoothManager.updateBluetoothIndication();
 
         // Update the phone state and other sensor/lock.
         mApplication.updatePhoneState(state);
