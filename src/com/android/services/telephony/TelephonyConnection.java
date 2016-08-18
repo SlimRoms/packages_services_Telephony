e1d655e038f9d1e35a5b159230518816719ca015
Qiang Chen <cqiang@codeaurora.org>
2016-08-18 18:31:40 +0800

    TeleService: Reset the duration after the CDMA call is accepted
    In android original design, the duration of CDMA MO call is started from
    the dial command sent, so it is not the real duration of the active time.
    
    In this patch, a new message is registered to listen the event of the call
    accepted, and then reset the duration when the event happens.
    
    CRs-Fixed: 754497
    
    Change-Id: Idae08bc17ad0044390ad8ebd34f7b4f29d35f7ed

--- src/com/android/services/telephony/TelephonyConnection.java
+++ src/com/android/services/telephony/TelephonyConnection.java
@@ -1238,7 +1238,7 @@ abstract class TelephonyConnection extends Connection {
         setActive();
     }
 
-    private void close() {
+    protected void close() {
         Log.v(this, "close");
         clearOriginalConnection();
         destroy();
