ad0a4d4381337b6c2eb0743aa2d73c69bb0b1e7f
Josue Rivera <prbassplayer@gmail.com>
2017-01-30 23:50:37 +0100

    Telephony: Uncondionally remove CallBarring preference
      * No one supports this
    
    Change-Id: I0df73d7053754bba3069feb77fab4f1118d4c775
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -355,10 +355,16 @@ public class CallFeaturesSetting extends PreferenceActivity
             /* tm.listen(mPhoneStateListener, PhoneStateListener.LISTEN_CALL_STATE); */
         }
 
+        // Remove callbarring as no one supports it
+        PreferenceScreen prefBarring = (PreferenceScreen)
+                prefSet.findPreference("button_callbarring_expand_key");
+        if (prefBarring != null) {
+                prefSet.removePreference(findPreference("button_callbarring_expand_key"));
+        }
+
         // Remove features if com.qualcomm.qti.ims app isn't found
         if (!PackageManagerUtils.isAppInstalled(this, "com.qualcomm.qti.ims")) {
             prefSet.removePreference(findPreference("ims_settings_key"));
-            prefSet.removePreference(findPreference("button_callbarring_expand_key"));
         }
 
         Preference wifiCallingSettings = findPreference(
