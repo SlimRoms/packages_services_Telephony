a404c73d27fab0854a0243c264fc528cfb99a86b
Griffin Millender <griffinn.millender@gmail.com>
2017-04-04 14:43:51 -0500

    Merge android-7.1.2_r2
    Conflicts:
    	res/values/config.xml
    	src/com/android/phone/settings/VoicemailSettingsActivity.java
    	src/com/android/services/telephony/TelephonyConnectionService.java
    
    Change-Id: I6facca62142d7f458fec4d0136410c78374870fb

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -1497,7 +1497,14 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     }
 
     public boolean setRadioPower(boolean turnOn) {
-        return setRadioPowerForSubscriber(getDefaultSubscription(), turnOn);
+        final Phone defaultPhone = PhoneFactory.getDefaultPhone();
+        if (defaultPhone != null) {
+            defaultPhone.setRadioPower(turnOn);
+            return true;
+        } else {
+            loge("There's no default phone.");
+            return false;
+        }
     }
 
     public boolean setRadioPowerForSubscriber(int subId, boolean turnOn) {
@@ -1728,6 +1735,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
 
     @Override
     public void setCellInfoListRate(int rateInMillis) {
+        enforceModifyPermission();
         mPhone.setCellInfoListRate(rateInMillis);
     }
 
